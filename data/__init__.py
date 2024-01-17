b06e2f9e11906ad97e75f7b8322b0b8c0b10cde6
Wang Jiarui <68880409+likakakaka@users.noreply.github.com>
2024-01-17 12:33:37 +0800

    Update __init__.py

--- data/__init__.py
+++ data/__init__.py
@@ -5,21 +5,21 @@ def create_dataloaders(args):
     """create dataloader"""
     if args.dataset == 'AID':
         from data.aid import AIDataset
-        training_set = AIDataset(args, root_dir='G:\datasets\yaogan\dataset\AID-dataset/train',
+        training_set = AIDataset(args, root_dir='.../AID-dataset/train',
                                  train=True)
-        test_set = AIDataset(args, root_dir='G:\datasets\yaogan\dataset/AID-dataset/val',
+        test_set = AIDataset(args, root_dir='.../AID-dataset/val',
                                    train=False)
     elif args.dataset == 'UCMerced':
         from data.ucmerced import UCMercedDataset
-        training_set = UCMercedDataset(args, root_dir='G:\datasets\yaogan\dataset/UCMerced-dataset/train',
+        training_set = UCMercedDataset(args, root_dir='.../UCMerced-dataset/train',
                                  train=True)
-        test_set = UCMercedDataset(args, root_dir='G:\datasets\yaogan\dataset/UCMerced-dataset/val',
+        test_set = UCMercedDataset(args, root_dir='.../UCMerced-dataset/val',
                                   train=False)
     elif args.dataset == 'NWPU':
         from data.nwpuresisc45 import NWPUDataset
-        training_set = NWPUDataset(args, root_dir='G:\datasets\yaogan\dataset/NWPU-RESISC45/train',
+        training_set = NWPUDataset(args, root_dir='.../NWPU-RESISC45/train',
                                  train=True)
-        test_set = NWPUDataset(args, root_dir='G:\datasets\yaogan\dataset/NWPU-RESISC45/val',
+        test_set = NWPUDataset(args, root_dir='.../NWPU-RESISC45/val',
                                   train=False)
     else:
         raise NotImplementedError(
